57204955dc4f03eaaca0d3e69522dbaf3c676e09
Reactodactyl <danayrferguson@gmail.com>
2022-12-02 21:25:54 +0800

    updated task table

--- dataAnalysisFuncts.py
+++ dataAnalysisFuncts.py
@@ -8,3 +8,33 @@
 # Best/worst ratings for user categs
 # Best/worst ratings for item categs
 # User relation to item item categs.
+
+Task	                                            Description	        Priority	Taskee
+Individual rating charts 
+for each user category	
+
+Best/Worst rated movies	
+
+Similarities within an item category
+
+Similarity within a specific user category 
+based on interest
+
+Similarities within a specific user category
+(age,gender,occupation)	
+
+Genre Frequency over the years
+
+Controversial movies??	
+
+(reserved) Data Preparation
+
+Implement the K-N-N algo
+
+Visualize the kNN algo
+
+Accuracy of  model	
+
+Precision of model
+
+Error of model			
